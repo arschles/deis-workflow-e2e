915d89cc1114beed41dafb2b3e574ee9062c28e8
Aaron Schlesinger <aschlesinger@deis.com>
2016-03-30 15:43:58 -0700

    fix(client/keys.go): add backup key ID to the SSH public key parser
    The parser uses this backup parameter if it can’t parse the key ID from
    the key itself

--- client/keys.go
+++ client/keys.go
@@ -38,14 +38,15 @@ func (e ErrUnknownSSHPubKeyID) Error() string {
 }
 
 // ParseSSHPubKey parses a byte slice representation of an SSH Public Key into an
-// SSHPubKeyInfo struct. Returns an appropriate error if parsing failed
-func ParseSSHPubKey(pubKey []byte) (*SSHPubKeyInfo, error) {
+// SSHPubKeyInfo struct. If it cannot find the key ID from the pubKey byte slice itself,
+// it uses backupKeyID instead. Returns an appropriate error if parsing failed.
+func ParseSSHPubKey(backupKeyID string, pubKey []byte) (*SSHPubKeyInfo, error) {
 	if !sshPubKeyRegex.Match(pubKey) {
 		return nil, ErrInvalidSSHPubKey{pubKey: pubKey}
 	}
 	capture := sshPubKeyRegex.FindStringSubmatch(string(pubKey))
 	if len(capture) < 4 || capture[3] == "" {
-		return nil, ErrUnknownSSHPubKeyID{pubKey: pubKey}
+		return &SSHPubKeyInfo{ID: backupKeyID, Public: string(pubKey)}, nil
 	}
 	return &SSHPubKeyInfo{ID: capture[3], Public: string(pubKey)}, nil
 }
